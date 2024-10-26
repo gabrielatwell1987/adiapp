791922709575eb5279946fbf48fc0005ece6dd54
gabrielatwell1987 <gabrielatwell@proton.me>
2024-10-26 15:17:54 -0700

    tried to make the app responsive

--- src/components/GitHub.svelte
+++ src/components/GitHub.svelte
@@ -3,7 +3,9 @@
 
     import { onMount } from 'svelte';
     import axios from 'axios';
-    import { Table, Button, Form, FormGroup, Input, Spinner } from 'sveltestrap';
+    import { Spinner } from 'sveltestrap';
+
+    const table = document.querySelector('tr');
 
     let users = [];
     let searchTerm = '';
@@ -30,39 +32,37 @@
     };
 </script>
 
-<img src="github.svg" alt="">
+<main>
+    
+    
+    <div class="content">
+        <img src="github.svg" alt="">
 
-<Form on:submit={handleSubmit}>
-    <FormGroup floating label="Search">
-        <Input type="text" bind:value={searchTerm} />
-    </FormGroup>
-    <Button type="submit" color="primary">Submit</Button>
-</Form>
+        <form on:submit={handleSubmit}>
+            <input type="text" bind:value={searchTerm} placeholder="search" />
 
-{#if isLoading}
-    <Spinner color="primary" />
-{/if}
+            <button type="submit">Submit</button>
+        </form>
 
-<Table>
-    <thead>
-        <tr>
-            <th>Id</th>
-            <th>Avatar</th>
-            <th>Login</th>
-            <th>URL</th>
-        </tr>
-    </thead>
-    <tbody>
-        {#each users as user}
-            <tr>
-                <th scope="row">{user.id}</th>
-                <td><img alt={user.login} src={user.avatar_url} width="75" height="75"></td>
-                <td><b>{user.login}</b></td>
-                <td><a href="{user.html_url}">{user.html_url}</a></td>
-            </tr>
-        {/each}
-    </tbody>
-</Table>
+        {#if isLoading}
+            <Spinner color="primary" />
+        {/if}
+
+        <div class="table">
+            <table>
+                <tbody>
+                    {#each users as user}
+                        <tr>
+                            <td><img alt={user.login} src={user.avatar_url} width="75" height="75"></td>
+                            <td><b>{user.login}</b></td>
+                            <td><a href="{user.html_url}">{user.html_url}</a></td>
+                        </tr>
+                    {/each}
+                </tbody>
+            </table>
+        </div>
+    </div>
+</main>
 
 <style>
     img {
@@ -71,8 +71,59 @@
         object-fit: cover;
         margin-bottom: 15%;
     }
+    
+    input {
+        width: 50%;
+        margin-inline: auto;
+        padding: 10px;
+        border-radius: 15px;
+        border: 1px solid #ccc;
+        margin-bottom: 10px;
+    }
 
-    th {
-        letter-spacing: 3px;
+    table {
+        position: relative;
+    }
+
+    .content {
+        scale: 1;
+        margin-inline: auto;
+    }
+
+    main {
+        position: relative;
+        margin-inline: auto;
+        width: 100%;
+        max-width: 400px;
+        text-align: center;
+    }
+
+    
+    @media (max-width: 500px) {
+        .table {
+            max-width: 100%;
+        }
+        
+        .content {
+            overflow-x: auto;
+            width: 99%;
+            scale: 1;
+            margin-inline: auto;
+            /* margin-right: 10%; */
+        }
+
+        table tr {
+            margin-inline: auto;
+        }
+
+        td {
+            margin: 0;
+            padding: 0;
+        }
+
+        input {
+            width: 100%;
+        }
     }
 </style>
+
